1e2f8d76cb823faff842cf48a76249bfe39121a6
manihamidi <manihamidi@gmail.com>
2017-12-23 15:31:57 -0800

    prepare mnist data and train in epochs

--- vae.py
+++ vae.py
@@ -1,5 +1,7 @@
 import torch
 from torch.optim import Adam
+from sklearn.datasets import fetch_mldata
+from tqdm import tqdm
 
 pz_size = 30
 image_size = 28 * 28
@@ -29,13 +31,30 @@ class VAE(torch.nn.Module):
         return self.output_layer(self.z)
 
 
-# normal distribution definition
-input_var = torch.autograd.Variable(x)
-vae = VAE()
-opim = Adam(vae.parameters(), lr=0.001, weight_decay=0.001)  # lr: learning rate, weight decay: ?
-output = vae(input_var)  # forward pass
-loss = loss_func(output, input_var)  # calculate loss
-loss.backward()  # back propagate loss to calculate the deltas (the "gradient")
-opim.step()  # use the learning rates and the gradient to update parameters
+def _prepare_data():
+    mnist = fetch_mldata('MNIST original', data_home='./')
+    mnist_tensor = torch.ByteTensor(mnist.data).type(torch.FloatTensor)
+    print(mnist_tensor.shape)
+    return mnist_tensor
 
-print(output)
+
+def train(data_var, epochs=5):
+    loss_trace = torch.zeros(epochs, data_var.shape[0])
+    for i in tqdm(range(epochs)):
+        for j in tqdm(range(data_var.shape[0])):
+            output = vae(data_var[j][:])  # forward pass
+            loss = loss_func(output, data_var[j][:])  # calculate loss
+            loss_trace[i][j] = loss.data[0]
+            loss.backward()  # back propagate loss to calculate the deltas (the "gradient")
+            optim.step()  # use the learning rates and the gradient to update parameters
+    print("loss.data: {}".format(loss.data))
+    return loss_trace
+
+
+vae = VAE()
+optim = Adam(vae.parameters(), lr=0.001, weight_decay=0.001)  # lr: learning rate, weight decay: ?
+mnist_tensor = _prepare_data()
+input_var = torch.autograd.Variable(mnist_tensor)
+trace = train(data_var=input_var)
+# normal distribution definition
+

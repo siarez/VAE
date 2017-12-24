ff6a479718fcdf1995bbe7a5569be13d90700b6c
manihamidi <manihamidi@gmail.com>
2017-12-24 01:06:20 -0800

    add relu, include kld in cost, widen hidden layers

--- vae.py
+++ vae.py
@@ -5,8 +5,8 @@ from tqdm import tqdm
 from logger import Logger
 import numpy as np
 
-pz_size = 20
-mid_size = 30
+pz_size = 40
+mid_size = 100
 image_size = 28 * 28
 
 x = torch.randn(image_size)
@@ -20,22 +20,34 @@ class VAE(torch.nn.Module):
         self.input_layer = torch.nn.Linear(image_size, mid_size)
         self.middle_layer = torch.nn.Linear(mid_size, pz_size)
         self.activation_layer = torch.nn.Sigmoid()  # TODO: can this be re-used? looks like it
+        self.relu_activate = torch.nn.ReLU()
         self.middle_out_layer = torch.nn.Linear(int(pz_size / 2), mid_size)
         self.output_layer = torch.nn.Linear(mid_size, image_size)
 
     def forward(self, x):
-        mu_sigma = self.activation_layer(self.middle_layer(self.input_layer(x)))
-        mu, sigma = torch.chunk(mu_sigma, 2)
+        mu_sigma = self.relu_activate(self.middle_layer(self.relu_activate(self.input_layer(x))))
+        mu, logvar = torch.chunk(mu_sigma, 2)
+        std = logvar.mul(0.5).exp()
 
         # Calculating hidden representation `z`
         normal_sample = torch.autograd.Variable(
             torch.normal(torch.zeros(int(pz_size / 2)), torch.ones(int(pz_size / 2))),
             requires_grad=False)
-        self.z = torch.mul(normal_sample, sigma) + mu
+        self.z = torch.mul(normal_sample, std) + mu
 
         # Calculating output
-        return self.activation_layer(self.output_layer(self.activation_layer(self.middle_out_layer(self.z))))
+        return self.activation_layer(self.output_layer(self.relu_activate(self.middle_out_layer(self.z)))), mu, logvar
 
+    def forward_classical(self, x):
+        mu_sigma = self.relu_activate(self.middle_layer(self.input_layer(x)))
+        mu, sigma = torch.chunk(mu_sigma, 2)
+        self.z = mu
+        # Calculating output
+        return self.activation_layer(self.output_layer(self.relu_activate(self.middle_out_layer(self.z))))
+
+
+def kl_loss(mu, logvar):
+    return -0.5*torch.sum(1 + logvar - mu.pow(2) - torch.exp(logvar))
 
 def _prepare_data():
     mnist = fetch_mldata('MNIST original', data_home='./')
@@ -52,14 +64,14 @@ def train(data_var, epochs=5):
 
     for i in tqdm(range(epochs)):
         for j in tqdm(range(data_var.shape[0])):
-            output = vae(data_var[j][:])  # forward pass
-            loss = loss_func(output, data_var[j][:])  # calculate loss
+            output, mu, logvar = vae(data_var[j][:])  # forward pass
+            loss = loss_func(output, data_var[j][:]) + kl_loss(mu, logvar) # calculate loss
             loss_trace[i][j] = loss.data[0]
             logger.scalar_summary("loss", loss.data[0], i + 1)
             loss.backward()  # back propagate loss to calculate the deltas (the "gradient")
             optim.step()  # use the learning rates and the gradient to update parameters
             if j % 5000 == 0:
-                log_images(data_var[j][:], output, i + 1)
+                log_images(data_var[j][:], output, (j+1) + data_var.shape[0]*(i + 1))
 
     return loss_trace
 
@@ -73,7 +85,7 @@ def log_images(input, output, epoch):
 logger = Logger("./logs")
 
 vae = VAE()
-optim = Adam(vae.parameters(), lr=0.00001, weight_decay=0.001)  # lr: learning rate, weight decay: ?
+optim = Adam(vae.parameters(), lr=0.00001)  # lr: learning rate, weight decay: ?
 mnist_tensor = _prepare_data()
 input_var = torch.autograd.Variable(mnist_tensor)
 trace = train(data_var=input_var)

4b2b2882b950fcf35eb471e3f7600c33d685e81d
sia <sia@siarez.com>
2017-12-24 19:56:22 -0800

    fixes major bugs -- zero_grad, KLD normalize, increase learning rate

--- vae.py
+++ vae.py
@@ -6,9 +6,10 @@ from tqdm import tqdm
 from logger import Logger
 import numpy as np
 
-BATCH_SIZE = 3
-pz_size = 40
-mid_size = 200
+EPOCHS = 10
+BATCH_SIZE = 35
+pz_size = 20
+mid_size = 400
 image_size = 28 * 28
 
 x = torch.randn(image_size)
@@ -26,29 +27,25 @@ class VAE(torch.nn.Module):
         self.relu_activate = torch.nn.ReLU()
         self.middle_out_layer = torch.nn.Linear(int(pz_size / 2), mid_size)
         self.output_layer = torch.nn.Linear(mid_size, image_size)
-        self.normal_tensor = torch.zeros(int(pz_size/2)).cuda()
+        self.normal_tensor = torch.zeros(BATCH_SIZE, int(pz_size/2)).cuda()
 
     def forward(self, x):
-        mu_sigma = self.relu_activate(self.middle_layer(self.relu_activate(self.input_layer(x))))
+        mu_sigma = self.middle_layer(self.relu_activate(self.input_layer(x)))
         mu, logvar = torch.chunk(mu_sigma, 2, dim=1)
         std = logvar.mul(0.5).exp()
 
         # Calculating hidden representation `z`
-        # normal_sample = torch.autograd.Variable(
-        #     torch.normal(torch.zeros(int(pz_size / 2)), torch.ones(int(pz_size / 2))),
-        #     requires_grad=False).cuda()
         self.normal_tensor.normal_()
-        # self.z = torch.mul(normal_sample, std) + mu
         self.z = torch.mul(torch.autograd.Variable(self.normal_tensor, requires_grad=False), std) + mu
         # Calculating output
         return self.activation_layer(self.output_layer(self.relu_activate(self.middle_out_layer(self.z)))), mu, logvar
 
     def forward_classical(self, x):
-        mu_sigma = self.relu_activate(self.middle_layer(self.input_layer(x)))
-        mu, sigma = torch.chunk(mu_sigma, 2)
+        mu_sigma = self.relu_activate(self.middle_layer(self.relu_activate(self.input_layer(x))))
+        mu, sigma = torch.chunk(mu_sigma, 2, dim=1)
         self.z = mu
         # Calculating output
-        return self.activation_layer(self.output_layer(self.relu_activate(self.middle_out_layer(self.z))))
+        return self.activation_layer(self.output_layer(self.relu_activate(self.middle_out_layer(self.z)))), mu, sigma
 
 class MyData(Dataset):
     def __init__(self, input):
@@ -62,7 +59,8 @@ class MyData(Dataset):
 
 
 def kl_loss(mu, logvar):
-    return -0.5*torch.sum(1 + logvar - mu.pow(2) - torch.exp(logvar))
+    # return 0
+    return -0.5*torch.sum(1 + logvar - mu.pow(2) - torch.exp(logvar)) / (BATCH_SIZE * pz_size)
 
 def _prepare_data():
     mnist = fetch_mldata('MNIST original', data_home='./')
@@ -76,21 +74,26 @@ def _prepare_data():
     return dataset
 
 
-def train(data_set, epochs=5):
+def train(data_set, epochs=EPOCHS):
     # loss_trace = torch.zeros(epochs, len(data_set))
     loss_trace = []
     data_loader = DataLoader(data_set, batch_size=BATCH_SIZE)
+    ii = 0
     for i in tqdm(range(epochs)):
         for batch_raw in tqdm(data_loader):
+            ii += 1
+            optim.zero_grad()
             batch = torch.autograd.Variable(batch_raw)
             output, mu, logvar = vae(batch)  # forward pass
-            loss = loss_func(output, batch) + kl_loss(mu, logvar) # calculate loss
+            loss = loss_func(output, batch) + kl_loss(mu, logvar) # calculate vaiational loss
+            #loss = loss_func(output, batch)  # calculate classical loss
             loss_trace.append(torch.sum(loss.data))
             logger.scalar_summary("loss", loss.data[0], i + 1)
             loss.backward()  # back propagate loss to calculate the deltas (the "gradient")
+
             optim.step()  # use the learning rates and the gradient to update parameters
-            # if j % 5000 == 0:
-            #     log_images(batch.data[], output, (j+1) + data_var.shape[0]*(i + 1))
+            if ii % 100 == 0:
+                log_images(batch[0], output[0], ii)
 
     return loss_trace
 
@@ -104,7 +107,7 @@ def log_images(input, output, epoch):
 logger = Logger("./logs")
 
 vae = VAE().cuda()
-optim = Adam(vae.parameters(), lr=0.00001)  # lr: learning rate, weight decay: ?
+optim = Adam(vae.parameters(), lr=0.001)  # lr: learning rate, weight decay: ?
 mnist_dataset = _prepare_data()
 trace = train(data_set=mnist_dataset)
 # normal distribution definition

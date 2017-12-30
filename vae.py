374eed61c40a5a0f4a3a91b3b0affee4d3a47478
siavash <sia@siarez.com>
2017-12-29 20:20:33 -0800

    json export and sampling for blog, refractoring

--- vae.py
+++ vae.py
@@ -5,11 +5,12 @@ from sklearn.datasets import fetch_mldata
 from tqdm import tqdm
 from logger import Logger
 import numpy as np
+import json
 
 EPOCHS = 6
 BATCH_SIZE = 35
 pz_size = 20
-mid_size = 400
+mid_size = 100
 image_size = 28 * 28
 # Check for CUDA
 cuda = torch.cuda.is_available()
@@ -20,18 +21,23 @@ class VAE(torch.nn.Module):
     def __init__(self):
         super(VAE, self).__init__()
         # Input output Layer definitions
-        self.input_layer = torch.nn.Linear(image_size, mid_size)
-        self.middle_layer = torch.nn.Linear(mid_size, pz_size)
-        self.activation_layer = torch.nn.Sigmoid()  # TODO: can this be re-used? looks like it
-        self.relu_activate = torch.nn.ReLU()
-        self.middle_out_layer = torch.nn.Linear(int(pz_size / 2), mid_size)
-        self.output_layer = torch.nn.Linear(mid_size, image_size)
+        self.encoder_input = torch.nn.Linear(image_size, mid_size)
+        self.encoder_hidden = torch.nn.Linear(mid_size, pz_size)
+        self.sigmoid = torch.nn.Sigmoid()  # TODO: can this be re-used? looks like it
+        self.relu = torch.nn.ReLU()
+        self.decoder_hidden = torch.nn.Linear(int(pz_size / 2), mid_size)
+        self.decoder_output = torch.nn.Linear(mid_size, image_size)
         self.normal_tensor = torch.zeros(BATCH_SIZE, int(pz_size / 2))
         if cuda:
             self.normal_tensor.cuda()
+        self.z = None
 
     def forward(self, x):
-        mu_logvar = self.middle_layer(self.relu_activate(self.input_layer(x)))
+        return self.forward_variational(x)
+        # return self.forward_vanilla(x)
+
+    def forward_variational(self, x):
+        mu_logvar = self.encoder_hidden(self.relu(self.encoder_input(x)))
         mu, logvar = torch.chunk(mu_logvar, 2, dim=1)
         std = logvar.mul(0.5).exp()
 
@@ -39,26 +45,56 @@ class VAE(torch.nn.Module):
         self.normal_tensor.normal_()
         self.z = torch.mul(torch.autograd.Variable(self.normal_tensor, requires_grad=False), std) + mu
         # Calculating output
-        return self.activation_layer(self.output_layer(self.relu_activate(self.middle_out_layer(self.z)))), mu, logvar
+        return self.sigmoid(self.decoder_output(self.relu(self.decoder_hidden(self.z)))), mu, logvar
 
-    def forward_classical(self, x):
+    def forward_vanilla(self, x):
         """Forward function for a regular vanilla autoencoder"""
-        mu_sigma = self.relu_activate(self.middle_layer(self.relu_activate(self.input_layer(x))))
-        mu, sigma = torch.chunk(mu_sigma, 2, dim=1)
-        self.z = mu
+        pz = self.relu(self.encoder_hidden(self.relu(self.encoder_input(x))))
+        # We cut pz in half
+        self.z, _ = torch.chunk(pz, 2, dim=1)
         # Calculating output
-        return self.activation_layer(self.output_layer(self.relu_activate(self.middle_out_layer(self.z)))), mu, sigma
+        return self.sigmoid(self.decoder_output(self.relu(self.decoder_hidden(self.z)))), self.z, None
+
+    def save_json(self):
+        """
+        Saves model parameters in JSON for use in deeplearning.js
+        :return: none
+        """
+        model_params = {
+            "enc_in_weight": {"param": self.encoder_input.weight.data.numpy().tolist(), "dim": self.encoder_input.weight.data.numpy().shape},
+            "enc_in_bias": {"param": self.encoder_input.bias.data.numpy().tolist(), "dim": self.encoder_input.bias.data.numpy().shape},
+            "enc_hid_weight": {"param": self.encoder_hidden.weight.data.numpy().tolist(), "dim": self.encoder_hidden.weight.data.numpy().shape},
+            "enc_hid_bias": {"param": self.encoder_hidden.bias.data.numpy().tolist(), "dim": self.encoder_hidden.bias.data.numpy().shape},
+            "dec_hid_weight": {"param": self.decoder_hidden.weight.data.numpy().tolist(), "dim": self.decoder_hidden.weight.data.numpy().shape},
+            "dec_hid_bias": {"param": self.decoder_hidden.bias.data.numpy().tolist(), "dim": self.decoder_hidden.bias.data.numpy().shape},
+            "dec_out_weight": {"param": self.decoder_output.weight.data.numpy().tolist(), "dim": self.decoder_output.weight.data.numpy().shape},
+            "dec_out_bias": {"param": self.decoder_output.bias.data.numpy().tolist(), "dim": self.decoder_output.bias.data.numpy().shape}
+        }
+        json_params = json.dumps(model_params)
+        text_file = open("model_params.json", "w")
+        text_file.write(json_params)
+        text_file.close()
 
 
 class MyData(Dataset):
-    def __init__(self, input):
+    def __init__(self, input, target):
         self.input = input
+        self.target = target
 
     def __len__(self):
         return self.input.shape[0]
 
     def __getitem__(self, item):
         return self.input[item]
+
+    def get_sample_digit(self, digit):
+        """A very simple function to retrieve a random mnist samples of the desired digit."""
+        sample_size = 100  # 100 should be big enough of a sample size to contain at least one of every digit.
+        random_offset = np.random.randint(0, len(self) - sample_size)
+        for idx in range(random_offset, random_offset + sample_size):
+            if self.target[idx] == digit:
+                return self.input[idx]
+        raise ValueError
 
 
 def kl_loss(mu, logvar):
@@ -69,7 +105,9 @@ def kl_loss(mu, logvar):
     :return: The sum of KL divergences between the distributions generated by the encoder and
     the “standard normal distribution” i.e. N(0, 1)
     """
-    scaling_factor = (logvar.shape[0] * image_size) * 10000
+    # Todo: this scaling factor was used by the implementation in pytorch repo's VAE example.
+    # Todo: Not sure of thr reasoning behind it.
+    scaling_factor = (logvar.shape[0] * image_size)
     KL_dive = -0.5*torch.sum(1 + logvar - mu.pow(2) - torch.exp(logvar))
     return KL_dive / scaling_factor
 
@@ -78,12 +116,14 @@ def _prepare_data():
     """Downloads (if necessary), randomizes and prepares the dataset."""
     mnist = fetch_mldata('MNIST original', data_home='./')
     mnist_tensor = torch.ByteTensor(mnist.data).type(torch.FloatTensor)
+    mnist_target = torch.ByteTensor(mnist.target)
     if cuda:
         mnist_tensor.cuda()
     rnd_idx = np.random.choice(len(mnist.data), len(mnist.data), replace=False)  # pick index values randomly
     mnist_tensor = mnist_tensor[rnd_idx, :]  # randomize the data set
+    mnist_target = mnist_target[torch.LongTensor(rnd_idx)]  # randomize the data set
     mnist_tensor /= 255  # normalize
-    dataset = MyData(mnist_tensor)
+    dataset = MyData(mnist_tensor, mnist_target)
     print(mnist_tensor.shape)
     return dataset
 
@@ -110,13 +150,15 @@ def train(data_set, epochs=EPOCHS, batch_size=BATCH_SIZE):
             optim.zero_grad()
             batch = torch.autograd.Variable(batch_raw)
             output, mu, logvar = vae(batch)  # forward pass
-            loss = loss_func(output, batch) + kl_loss(mu, logvar)  # calculate loss
+            # calculate loss. We don't calculate KL_loss for vanilla autoencoder (i.e. logvar is None)
+            loss = loss_func(output, batch) + (kl_loss(mu, logvar) if logvar is not None else 0)
             logger.scalar_summary("loss", loss.data[0], epoch * len(data_loader) + ii)
             loss.backward()  # back propagate loss to calculate the deltas (the "gradient")
             optim.step()  # use the learning rates and the gradient to update parameters
-            if ii % 100 == 0:
+            if ii % 100 == 0 and False:
                 # logging images for viewing Tensorboard
                 log_images("Training", batch[0], output[0], ii)
+                '''
                 # running the validation dataset
                 val_loss = 0
                 num_of_batches = 0
@@ -125,14 +167,27 @@ def train(data_set, epochs=EPOCHS, batch_size=BATCH_SIZE):
                     output, mu, logvar = vae(val_image_var)  # forward pass
                     val_loss += loss_func(output, val_image_var) + kl_loss(mu, logvar)  # calculate loss TODO: Should we add the regularizer term for validation?
                     num_of_batches += 1
+                log_images("Validation", val_image_var[0], output[0], ii)
                 logger.scalar_summary("val_loss", val_loss.data[0]/num_of_batches, epoch * len(data_loader) + ii)
+                '''
+
 
 
 logger = Logger("./logs")
 vae = VAE()
+
 if cuda:
     vae.cuda()
 optim = Adam(vae.parameters(), lr=0.001)  # lr: learning rate, weight decay: ?
 mnist_dataset = _prepare_data()
 train(data_set=mnist_dataset)
+vae.save_json()
+# Get some sample images and their z representation for the blog
+z_representaions = []
+for digit in range(0, 10):
+    image = mnist_dataset.get_sample_digit(digit)
+    image.unsqueeze_(0)
+    output, mu, logvar = vae(torch.autograd.Variable(image))
+    z_representaions.append(mu.data.numpy().tolist()[0])
+print(json.dumps(z_representaions))
 
